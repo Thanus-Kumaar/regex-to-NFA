2b481c1d992b377e908f90f8abaa872149829039
Abishek-45 <abishek13105@gmail.com>
2024-03-10 23:39:45 +0530

    Added a file for checking the RegexExpression and validating it . Different files where created for each purposes and in the algorithm file , all the other file's modules and functions were imported

---
+++ RegexCheck.py
@@ -0,0 +1,63 @@
+class Token:
+    def __init__(self, value_type, value):
+        self.type = value_type
+        self.value = value
+
+def lexical_analysis(input_str):
+    tokens = []
+    index = 0
+
+    while index < len(input_str):
+        value = input_str[index]
+
+        if value.isalnum() or value in "ϵ?.":
+            tokens.append(Token("variables", value))
+        elif value in "*+":
+            tokens.append(Token("uni-operators", value))
+        elif value == "|":
+            tokens.append(Token("binary-operator", value))
+        elif value == "(":
+            tokens.append(Token("group_start", value))
+        elif value == ")":
+            tokens.append(Token("group_end", value))
+        else:
+            return("Error: Invalid input at position:", index + 1)
+        
+        index += 1
+
+    return parser(tokens,input_str)
+
+    
+
+def parser(tokens,input_str):
+    stack = []
+    count=0
+
+    
+    for i in range(len(tokens)):
+        if tokens[i].type == "variables":
+            if tokens[i].value == "?" and count==0:
+                    return "Error: Unexpected", tokens[i].value, "at", i
+            count+=1
+            pass
+        elif tokens[i].type == "uni-operators":
+            if i == 0 or tokens[i - 1].type in ["group_start", "binary-operator","uni-operators"]:
+                return "Error: Unexpected", tokens[i].value, "at", i
+        elif tokens[i].type == "binary-operator":
+            if i == 0 or i == len(tokens) - 1 or tokens[i - 1].type in ["group_start", "binary-operator"] or \
+                    tokens[i + 1].type in ["group_end", "uni-operators"]:
+                return "Error: Unexpected", tokens[i].value, "at", i
+        elif tokens[i].type == "group_start":
+            stack.append(i)
+        elif tokens[i].type == "group_end":
+            if not stack:
+                return "Error: Unmatched closing parenthesis at", i
+            stack.pop()
+
+    if stack:
+        return "Error: Unmatched opening parenthesis at", stack[-1] + 1
+
+    if count>0:
+        return input_str
+    else:
+        return "Error: No variables"
--- algorithm.py
+++ algorithm.py
@@ -1,2 +1,18 @@
-regex = input("Enter regex expression")
+import modifiedDoublyLinkedList, RegexCheck
 
+alphaSet = input("Enter alphabet set: ")
+MDLL = modifiedDoublyLinkedList(alphaSet)
+regex_code = input("Enter regex expression: ")
+validated_regex_code=RegexCheck.lexical_analysis(regex_code)
+
+if validated_regex_code == regex_code :
+    for i in regex_code:
+        MDLL.addNode(i)
+
+    MDLL.setFinalState()
+
+    MDLL.printList(MDLL.head)
+
+else:
+
+    print(validated_regex_code)
